f0d989b51cad3a9f8d8799e447f7d7239191e7ca
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-01-17 15:52:02 +0530

    ak

--- ak.C
+++ ak.C
@@ -1,16 +1,13 @@
 #include <stdio.h>
-int main()
+#include<conio.h>
+void main()
 {
-    int number;
-
-    printf("Enter an integer: ");
-    scanf("%d", &number);
-
-   
-    if(number % 2 == 0)
-        printf("%d is even.", number);
-    else
-        printf("%d is odd.", number);
-
-    return 0;
+int num;
+printf("Enter an integer: ");
+scanf("%d", &num);
+if(num%2==0)
+printf("%d is even.",num);
+else
+printf("%d is odd.",num);
+getch();
 }
