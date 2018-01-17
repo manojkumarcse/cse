b73ee81fe65476b1ace4e331a0df282ed2f4ab44
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-01-17 15:58:44 +0530

    mk
    learn more

--- mk.c
+++ mk.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-int main()
+#include<conio.h>
+void main()
 {
-    double number;
-
-    printf("Enter a number: ");
-    scanf("%lf", &number);
-
-    if (number <= 0.0)
-    {
-        if (number == 0.0)
-            printf("You entered 0.");
-        else
-            printf("You entered a negative number.");
-    }
-    else
-        printf("You entered a positive number.");
-    return 0;
+double num;
+printf("Enter a num: ");
+scanf("%lf", &num);
+if (num <= 0.0)
+{
+if (num == 0.0)
+printf("You entered 0.");
+else
+printf("You entered a negative num.");
+}
+else
+printf("You entered a positive num.");
+getch();
 }
